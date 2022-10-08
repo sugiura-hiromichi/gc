f81b9ede6a9525c60663757bfb938b81d053955e
ah-y <sugiura130418@icloud.com>
2022-10-08 18:44:36 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
+use mylibrary::cli::CliParser;
 use mylibrary::sh_cmd;
 use std::env::args;
 
 fn main() {
-   let mut arg = args();
-   arg.next();
-   let mut url: String = arg.collect();
+   let mut url = args().to_string();
    if url.find("/",).is_none() {
       url = format!("sugiura-hiromichi/{url}");
    }
    url = format!("https://github.com/{url}");
 
    sh_cmd!("git", ["clone".to_string(), url]);
-}
-
-#[cfg(test)]
-mod tests {
-   use super::*;
-
-   #[test]
-   fn arg_tes() {
-      for arg in args() {
-         println!("{arg}");
-      }
-   }
 }

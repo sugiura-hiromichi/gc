e94ac593dc49b87ad4bf5c62c4b605676d50b39d
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-08 18:36:23 +0900

    update(sh_cmd)

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use mylibrary::sh_cmd;
 use std::env::args;
 
 fn main() {
-   let mut url = args().to_string();
-   if url.find("/",).is_none() {
-      url = format!("sugiura-hiromichi/{url}");
-   }
-   url = format!("https://github.com/{url}");
+	let mut url = args().to_string();
+	if url.find("/",).is_none() {
+		url = format!("sugiura-hiromichi/{url}");
+	}
+	url = format!("https://github.com/{url}");
 
-   sh_cmd!("git", ["clone".to_string(), url]);
+	sh_cmd!("git", ["clone".to_string(), url.clone()]);
 }

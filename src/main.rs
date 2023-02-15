2f2eef330cf4276232819387419d83875b9613e5
sugiura-hiromichi <sugiura130418@icloud.com>
2023-02-15 10:27:47 +0900

    update(dep)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use mylibrary::cli::CliParser;
-use mylibrary::sh_cmd;
 use std::env::args;
+use sugiura_hiromichi_mylibrary::cli::CliParser as _;
+use sugiura_hiromichi_mylibrary::sh_cmd;
 
-fn main() {
+fn main() -> anyhow::Result<(),> {
 	let mut url = args().to_string();
 	if url.find("/",).is_none() {
 		url = format!("sugiura-hiromichi/{url}");
 	}
 	url = format!("git@github.com:{url}");
 
-	sh_cmd!("git", ["clone", "--depth", "1", &url]);
+	let o = sh_cmd!("git", ["clone", "--depth", "1", &url])?.unwrap();
+	println!("exit_status |>{}", o.status);
+	Ok((),)
 }
